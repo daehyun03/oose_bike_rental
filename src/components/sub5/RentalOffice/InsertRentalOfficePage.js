0ff11d47e0e0e2578d37dc8564dee9ea96487795
PolarLeesangho <96410972+PolarLeesangho@users.noreply.github.com>
2023-06-08 15:45:28 +0900

    sub5 UI 구현 완료

--- src/components/sub5/RentalOffice/InsertRentalOfficePage.js
+++ src/components/sub5/RentalOffice/InsertRentalOfficePage.js
@@ -1,26 +1,33 @@
-
+import React from 'react';
+import {Link } from 'react-router-dom';
 
 function InsertRentalOfficePage(){
 
     return(
-        <div className="InsertRentalOfficePage">
+        <div className = "container">
+            
             <div className="header">
-                <h1>대여소 등록</h1>
+                <h2>대여소 등록</h2>
             </div>
             <div className="body">
-                <div className = "InsertID">
+                <div className = "Insert_box">
                     <span>대여소 ID:</span>
                     <input type="text" placeholder="대여소 ID 입력" />
+                    
                 </div>
-                <div className = "InsertName">
+                <div className = "Insert_box">
                     <span>대여소 이름:</span>
                     <input type="text" placeholder="대여소 이름 입력"/>
                 </div>
-                <div className = "managerID">
+                <div className = "Insert_box">
                     <span>관리자 이름:</span>
                     <input type="text" placeholder="관리자 ID 입력" />
                 </div>
-                <button className="InsertBtn">등록</button>
+                <button className="startBtn Btn">등록</button>
+                <Link to="/rentalOffice">
+                    <button className="backBtn Btn">뒤로</button>
+                </Link>
+                
             </div>
         </div>
     );
