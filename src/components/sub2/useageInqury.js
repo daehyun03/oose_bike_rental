c623d3cc38a2fbd45c4a18d90ded5e636241d8db
asei1 <leesm0466@naver.com>
2023-06-10 08:01:21 +0900

    sub2 수정

--- src/components/sub2/useageInqury.js
+++ src/components/sub2/useageInqury.js
@@ -1,22 +1,49 @@
-import Table from "./Table"
-import "./useageInqury.css"
-import Paging from "./Paging";
+import { useEffect } from 'react';
+import './useageinqury.css';
 
 
-export default function useageInqury() {
-    const dummy = [
-        {No: 1, Title: "대여1"},
-        {No: 2, Title: "대여2"},
-        {No: 3, Title: "대여3"},
-        {No: 4, Title: "대여4"}
-    ]
+function useageInqury({match}) {
+    const idx = match.params.idx;
+    console.log('idx :: ', idx)
+    console.log('paymentHistory :: ', paymentHistory)
+    console.log('useageHistory :: ', useageHistory)
 
+ 
+	// 데이터를 호출해 오는 동안 대기할 수 있도록 async, await 사용
+    useEffect(async() => {
+        try{
+        	// '/api/BoardContent' 라는 uri 로 DB를 불러온다.
+            const res = await axios.get('/api/BoardContent', {
+                params: {
+                    'idx': idx,
+                    'paymentHistory' : paymentHistory,
+                    'useageHistory' : useageHistory
+                }
+            })
+        } catch(e) {
+            console.error(e.message)
+        }
+    },[])
+ 
     return(
-        <div className="container">
-            <h2>이용 내역 조회</h2>
-            <Table data={dummy}></Table>
+        <div>
+            <div class = "container">
+            <h2>자전거 대여기록</h2>
             <div className="button_container">
+            <div>
+                <div>
+                    <label for='idx'>일련번호</label>
+                </div>
+                <div>
+                    <label for='paymentHistory'>결제기록</label>
+                </div>
+                <div>
+                    <label for='useageHistory'>대여일</label>
+                </div>
             </div>
         </div>
+    </div>
+    </div>
     )
 }
+export default useageInqury;
