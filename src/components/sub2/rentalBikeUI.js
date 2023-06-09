7b20f456ecc8195bb207efe0281241066e2d1099
asei1 <leesm0466@naver.com>
2023-06-09 12:17:56 +0900

    sub2_수정

---
+++ src/components/sub2/rentalBikeUI.js
@@ -0,0 +1,16 @@
+import "./rentalBikeUI.css"
+import {Link} from "react-router-dom";
+
+
+function rentalBikeUI() {
+    return (
+        <div className="container">
+            <h2>자전거 대여 페이지</h2>
+           <div className="button_container">
+            <Link to="/rentalBike"><button className="rental_button">자전거 대여</button></Link>
+           </div>
+        </div>
+    );
+}
+
+export default rentalBikeUI;
