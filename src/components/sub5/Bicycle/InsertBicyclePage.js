d0a5eb4b2de5fdb12bc6de6486045755aa7fe8df
상호 이 <2000rhehcl@naver.com>
2023-06-10 23:43:33 +0900

    sub5 기능 추가

--- src/components/sub5/Bicycle/InsertBicyclePage.js
+++ src/components/sub5/Bicycle/InsertBicyclePage.js
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {Link } from 'react-router-dom';
 
 function InsertBicyclePage(){
+    const [id, setId] = useState('');
 
+    const handleRegister = (e) => {
+        console.log(id);
+        const Bicycle = {
+            Bicycle : ({
+                id : id,
+            })
+        }
+        fetch('http://localhost:3001/insertBicycle', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(Bicycle),
+        });
+    };
+    
     return(
         <div className = "container">
             <div className="header">
                 <h2>자전거 등록</h2>
             </div>
             <div className="body">
-                <div className = "Insert_box">
+                <form className = "Insert_box" role = "form" onSubmit={handleRegister}>
                     <span className = "text">자전거 ID:</span>
-                    <input type="text" placeholder="자전거 ID 입력" />
-                </div>
+                    <input type="text" placeholder="자전거 ID 입력" onChange={(e)=>setId(e.target.value)}/>
+                </form>
                 <button className="startBtn Btn">등록</button>
                 <Link to="/bicycle">
                     <button className="backBtn Btn">뒤로</button>
--- src/components/sub5/RentalOffice/InsertRentalOfficePage.js
+++ src/components/sub5/RentalOffice/InsertRentalOfficePage.js
@@ -2,33 +2,54 @@ import React from 'react';
 import {Link } from 'react-router-dom';
 
 function InsertRentalOfficePage(){
+    const [id, setId] = useState('');
+    const [name, setName] = useState('');
+    const [manager, setManager] = useState('');
+
+    const handleRegister = (e) => {
+        console.log(id);
+        const rentalOffice = {
+            rentalOffice : ({
+                id : id,
+                name : name,
+                manager : manager,
+            })
+        }
+
+        fetch('http://localhost:3001/insertRentalOffice', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(rentalOffice),
+        });
+    };
+
 
     return(
-        <div className = "container">
-            
+        <div className = "container">            
             <div className="header">
                 <h2>대여소 등록</h2>
             </div>
-            <div className="body">
-                <div className = "Insert_box">
-                    <span>대여소 ID:</span>
-                    <input type="text" placeholder="대여소 ID 입력" />
-                    
+            <form className="body">
+                <div className = "Insert_box" role="form" onSubmit={handleRegister} >
+                    <span>대여소 ID : </span>
+                    <input type="text" placeholder="대여소 ID 입력" onChange={(e) => setId(e.target.value)} required />
                 </div>
                 <div className = "Insert_box">
-                    <span>대여소 이름:</span>
-                    <input type="text" placeholder="대여소 이름 입력"/>
+                    <span>대여소 이름 : </span>
+                    <input type="text" placeholder="대여소 이름 입력" onChange={(e) => setName(e.target.value)} required />
                 </div>
                 <div className = "Insert_box">
-                    <span>관리자 이름:</span>
-                    <input type="text" placeholder="관리자 ID 입력" />
+                    <span>관리자 ID : </span>
+                    <input type="text" placeholder="관리자 ID 입력" onChange={(e) => setManager(e.target.value)} required />
                 </div>
                 <button className="startBtn Btn">등록</button>
                 <Link to="/rentalOffice">
                     <button className="backBtn Btn">뒤로</button>
                 </Link>
                 
-            </div>
+            </form>
         </div>
     );
 }
