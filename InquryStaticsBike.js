ef01abdb2db9c40c08c2e546209b9036921b9ad9
ssk33 <82458049+cmss0330813@users.noreply.github.com>
2023-06-13 21:59:12 +0900

    Add files via upload

--- InquryStaticsBike.js
+++ InquryStaticsBike.js
@@ -1,16 +1,50 @@
-import "./InquryStaticsBike.css"
-import {useParams} from "react-router-dom"
+import { patch } from "../sub3Control";
 
-export default function () {
-    const statics = useParams();
+import "./InquryStaticsBike.css";
+import { useEffect, useState, useCallback } from "react"
+
+function InquryStaticsBike () {
+    const [stast, setStast] = useState([
+        idx = "",
+        state = "",
+        locate = ""
+    ]);
+
+    useEffect(() => {
+        const res = 
+        fetch('https://localhost:8080/InquryStaticsBike', {
+            method: 'post',
+            body: JSON.stringify({
+                idx: 1
+            })
+          })
+          .then(res => res.json())
+          .then(res => {
+            if (res.success) {
+                alert("저장 완료");
+            }
+          })
+    }, [])
+   
+      const stastList = stast.map((data) => (
+      <form className="contents_value">
+        <div className="contents_id">{stast.idx}</div>
+        <div className="contents_state">{stast.state}</div>
+        <div className="content_locate">{stast.location}</div>
+      </form>));
+
+
     return(
         <div className="container">
-            <h2>자전거 통계 조회{statics.no}</h2>
+            <h2>자전거 통계 조회</h2>
             <form className="contents"> 
-                <div className="bicycle_num">자전거 일련번호</div>
-                <div className="bicycle_locate">위치</div>
-                <div className="bicycle_date">대여 날짜</div>
+                <div className="bicycle_id">자전거 일련번호</div>
+                <div className="bicycle_state">자전거 상태</div>
+                <div className="bicycle_locate">자전거 위치</div>
             </form>
+            {stastList}
         </div>
     );
 }
+
+export default InquryStaticsBike;
