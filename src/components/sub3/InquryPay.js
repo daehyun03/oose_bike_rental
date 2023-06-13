8b4b8e176cfa04754ddf181f05a5ecfae8cde94c
ssk33 <82458049+cmss0330813@users.noreply.github.com>
2023-06-13 22:01:11 +0900

    Update InquryPay.js

--- src/components/sub3/InquryPay.js
+++ src/components/sub3/InquryPay.js
@@ -1,29 +1,51 @@
+import { patch } from "../sub3Control";
 import "./InquryPay.css"
-//import {useParams} from "react-router-dom"
+import { useEffect, useState, useCallback } from "react"
 
-function InquryPay() {
+async function InquryPay() {
+    const [payment, setPay] = useState("");
+    const [userId, setUserId] = useState(0);
+
+    useEffect(() => {
+        const res = 
+        fetch('https://localhost:8080/InquryPay', {
+            method: 'post',
+            body: JSON.stringify({
+                idx: 1
+            })
+          })
+          .then(res => res.json())
+          .then(res => {
+            if (res.success) {
+                alert("저장 완료");
+            }
+          })
+    }, [])
 
     return(
         <div className="container">
             <h2>결제 내역 조회</h2> 
+
+            <div className="inputID">
+                <div className="text">사용자의 id를 입력해주세요</div>
+                <input id="userId">{userId}</input>
+            </div>
+
             <div className="contents"> 
                 <div className="idx">일련번호
-                    <div className="content_idx">######</div>
+                    <div className="content_idx">{payment.idx}</div>
                 </div>
                 <div className="method">결재 방식
-                    <div className="content_method">휴대폰 결제</div>
+                    <div className="content_method">{payment.method}</div>
                 </div>
                 <div className="isEasy">간편결제 여부
-                    <div className="content_isEasy">N</div>
+                    <div className="content_isEasy">{payment.isEasyPayment}</div>
                 </div>
-                <div className="extraPay">잔여 결제금
-                    <div className="content_extraPay">0</div>
+                <div className="extraPay">미납금
+                    <div className="content_extraPay">{payment.extraPayment}</div>
                 </div>
             </div>
-        </div>
-        
-        
+        </div>      
     );
 }
-
 export default InquryPay;
