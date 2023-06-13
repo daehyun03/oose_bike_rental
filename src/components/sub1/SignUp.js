860126f8d31671c0c4d900d560d3015ff92e4bbe
ilohas0021 <ilohas0021@naver.com>
2023-06-13 14:23:26 +0900

    DB 커넥션 분리 (진행 중), S1 라우터 분리

--- src/components/sub1/SignUp.js
+++ src/components/sub1/SignUp.js
@@ -20,7 +20,7 @@ const SignUp = () => {
             })
         }
 
-        fetch('http://localhost:3001/insertUser', {
+        fetch('http://localhost:3001/sub1Control/insertUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
