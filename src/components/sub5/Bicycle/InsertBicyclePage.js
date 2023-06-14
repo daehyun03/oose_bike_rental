a16934943fea144e39556be019929604820f3263
상호 이 <2000rhehcl@naver.com>
2023-06-14 21:24:20 +0900

    기능 수정 완료, DB미연결로 확인x

--- src/components/sub5/Bicycle/InsertBicyclePage.js
+++ src/components/sub5/Bicycle/InsertBicyclePage.js
@@ -3,36 +3,42 @@ import {Link } from 'react-router-dom';
 import '../test.css';
 import '../Sub5.css';
 
+function onSubmit(id) {
+     
+    const Bicycle = {
+        Bicycle : ({
+            id : id,
+        })
+    }
+
+    console.log(Bicycle);
+    fetch('http://localhost:3001/sub5Control/insertBicycle', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(Bicycle),
+    });
+    alert("자전거 등록!")
+}
+
 function InsertBicyclePage(){
     const [id, setId] = useState('');
 
-    const handleRegister = (e) => {
-        console.log(id);
-        const Bicycle = {
-            Bicycle : ({
-                id : id,
-            })
-        }
-        fetch('http://localhost:3001/insertBicycle', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(Bicycle),
-        });
-    };
-    
+    function idChange(e) {
+        setId(e.target.value)
+    }
     return(
         <div className = "container">
             <div className="header">
                 <h2>자전거 등록</h2>
             </div>
             <div className="body">
-                <form className = "Insert_box" role = "form" onSubmit={handleRegister}>
+                <form className = "Insert_box" role = "form">
                     <span className = "text">자전거 ID:</span>
-                    <input type="text" placeholder="자전거 ID 입력" onChange={(e)=>setId(e.target.value)}/>
+                    <input type="text" placeholder="자전거 ID 입력" onChange={(e)=>idChange(e)}/>
                 </form>
-                <button className="startBtn Btn">등록</button>
+                <button className="startBtn Btn"onClick={() => onSubmit(id)}>등록</button>
                 <Link to="/bicycle">
                     <button className="backBtn Btn">뒤로</button>
                 </Link>
