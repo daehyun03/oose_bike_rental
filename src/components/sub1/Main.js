6dbe63ea3805caccc8ba2137f4dbaccc4b8eca56
ilohas0021 <ilohas0021@naver.com>
2023-06-16 21:12:53 +0900

    Main Page

---
+++ src/components/sub1/Main.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Main.css';
+
+const Main = () => {
+    return (
+            <div className="container">
+                <h2>기장군 공영자전거 무인대여시스템</h2>&nbsp;
+                <div className="container_box">
+                    <div className="slider_overlay">
+                        <div className="carousel-caption">
+                            <div className="slider_text">
+                                <h3>책임</h3>
+                                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                <h3>의무</h3>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    )};
+
+export default Main;
