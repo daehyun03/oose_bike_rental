cd2237e479e2ac4036bf521adcf3fb08b96eed29
daehyun03 <kimdeahyeon5445@gmail.com>
2023-06-18 18:40:47 +0900

    .

--- server/dbConn.js
+++ server/dbConn.js
@@ -3,8 +3,8 @@ const mysql = require("mysql");
 const connection = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Aadmin972",
-    database: "db2",
+    password: "1234",
+    database: "bike_rental",
 });
 
 module.exports = connection;
